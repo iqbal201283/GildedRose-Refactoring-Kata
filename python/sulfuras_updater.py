ffce34049f2b5ac7a0b22ab585f1c1cae644f637
iqbal201283 <iqbal201283@gmail.com>
2025-06-25 07:48:26 +0530

    created a new class sulfuras_updater for Refactoring

---
+++ python/sulfuras_updater.py
@@ -0,0 +1,8 @@
+class SulfurasUpdater(BaseItemUpdater):
+    def update(self):
+        print("Sulfuras has no change")
+    def update_quality(self):
+        print("Sulfuras has no change")
+        # pass  
+    def update_sell_in(self):
+        print("Sulfuras has no change")
